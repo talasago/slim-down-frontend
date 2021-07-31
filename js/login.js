38603b9c6fd86c3e24fc66d3ec73f8d9e39c25f0
talasago <lyricaldragons@gmail.com>
2021-07-31 19:59:20 +0900

    feat: ログイン実装

---
+++ js/login.js
@@ -0,0 +1,55 @@
+function cognitoLogin() {
+    function toUsername(email) {
+        return email.replace('@', '-at-');
+    }
+
+    let poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.clientId
+    };
+    let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    let attributeList = [];
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: toUsername(email),
+        Password: password
+    })
+
+    let cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+        Username: toUsername(email),
+        Pool: userPool
+    });
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        //認証成功
+        onSuccess: function(result) {
+            let accessToken = result.getAccessToken().getJwtToken();
+            console.log("accessToken : " + accessToken);
+
+            AWS.config.region = _config.cognito.region;
+            let login_key = `cognito-idp.${_config.cognito.region}.amazonaws.com/${_config.cognito.userPoolId}`
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId: `${_config.cognito.userPoolId}`,
+                Logins: {
+                    [login_key] : result.getIdToken().getJwtToken(),
+                },
+            });
+
+            AWS.config.credentials.refresh(error => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    // example: let s3 = new AWS.S3();
+                    console.log('Successfully logged!');
+                }
+            });
+        },
+
+        onFailure: function(err) {
+            alert(err.message || JSON.stringify(err));
+        },
+    });
+}
