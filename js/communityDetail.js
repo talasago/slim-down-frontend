cadd794d496def780ad8b5375c52a6988772ac10
talasago <lyricaldragons@gmail.com>
2021-10-23 09:29:18 +0900

    feat: コミュニティ詳細作成

---
+++ js/communityDetail.js
@@ -0,0 +1,56 @@
+let id_token = localStorage.getItem("idToken");
+
+window.onload = function communityReed(){
+    let queryParam = window.location.search;
+    let endpoint = `${_config.endpoint.commutityInfo}/${queryParam}`;
+    let params = {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            Authorization: id_token
+        },
+    };
+
+    fetch(endpoint, params)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("データ読取時にエラーが発生しました");
+            }
+        })
+        .then((response_body) => {
+            readContentScreenApply(response_body);
+        })
+        .catch(error => {
+            alert("データ読取時にエラーが発生しました");
+            console.log(error);
+        });
+
+    function readContentScreenApply(response_body){
+        //itemsがなれけば処理を抜ける
+        if (!Object.keys(response_body).length) {
+            return null;
+        }
+
+        //一覧作成
+        let trs = document.createDocumentFragment();
+        for (let item of response_body.items) {
+            let tr = document.createElement("tr");
+            let tds = document.createDocumentFragment();
+            let a = document.createElement("a")
+
+            // column
+            for (let key in item) {
+                let td = document.createElement("td");
+                td.appendChild(document.createTextNode(item[key]));
+                tds.append(td);
+            }
+            a.href = `./community_detail.html?communityId=${item["communityId"]}`
+            a.innerHTML = "詳細"
+            tr.append(tds, a);
+            trs.append(tr);
+        }
+        document.getElementById('TbodyCommunityList').append(trs);
+    }
+}
