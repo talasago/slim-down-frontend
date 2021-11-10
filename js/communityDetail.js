07486cd555a976cce99a41ab8e8358532ca879fb
talasago <lyricaldragons@gmail.com>
2021-11-10 21:05:28 +0900

    feat: コミュニティ更新実装

--- js/communityDetail.js
+++ js/communityDetail.js
@@ -1,6 +1,7 @@
 const ID_TOKEN = localStorage.getItem("idToken");
 const LOGIN_USER_SUB = localStorage.getItem("sub")
 
+//queryParamがないときは新規登録扱いでいい
 window.onload = function communityReed(){
     let queryParam = window.location.search;
     let endpoint = `${_config.endpoint.commutityInfo}/${queryParam}`;
@@ -30,12 +31,14 @@ window.onload = function communityReed(){
 
     function readContentScreenApply(response_body){
         // APIで取得した内容を設定
-
-        let input_list = ['communityId', 'communityName', 'content']
+        let input_list = ['communityName', 'content']
         let div_list = ['communityOwner', 'weight', 'createdAt', 'updatedAt']
 
         //コミュニティ作成者がログインしたユーザーの場合
         if (response_body.communityOwner === LOGIN_USER_SUB) {
+            document.getElementById("btnUpdateCommunity").hidden = false;
+            document.getElementById("btnDeleteCommunity").hidden = false;
+
             for (let body_key of Object.keys(response_body)) {
                 if (input_list.includes(body_key)) {
                     document.getElementById(body_key).value = response_body[body_key];
@@ -45,5 +48,48 @@ window.onload = function communityReed(){
                 }
             }
         }
+    }
+}
+
+function updateCommunity(){
+    let endpoint = `${_config.endpoint.commutityInfo}/`;
+    let data = {
+        communityId: document.getElementById("communityId").value,
+        communityName: document.getElementById("communityName").value,
+        content: document.getElementById("content").value
+    };
+    let params = {
+        method: "PUT",
+        mode: 'cors',
+        headers: {
+            Authorization: ID_TOKEN
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch(endpoint, params)
+        .then(response => {
+            if (response.ok) {
+                alert("更新しました");
+            } else {
+                alert("データ読取時にエラーが発生しました");
+            }
+        })
+        .catch(error => {
+            alert("データ読取時にエラーが発生しました");
+            console.log(error);
+        });
+}
+
+function deleteCommunity(){
+
+}
+
+function dispAlertForCommunity(type){
+    if (type === "join") {
+        // アラートでok,ng
+        // okならjoinのAPI叩く
+    } else if (type == "leave") {
+
     }
 }
