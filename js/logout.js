90b8e09140d4e9055bf436e536462d45c2b464b1
talasago <lyricaldragons@gmail.com>
2021-12-25 08:38:07 +0900

    fix: ログアウト

--- js/logout.js
+++ js/logout.js
@@ -3,6 +3,5 @@ function logout() {
     localStorage.removeItem("sub");
     localStorage.removeItem("accessToken");
     localStorage.removeItem("userBelongCommunityId");
-    alert('ログアウトしました')
     window.location.href = "./login.html";
 };
